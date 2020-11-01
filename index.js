e8536ed8d1c7c18ce69c36ee79799c365b78f7fd
007steve <stephen.plummer1.sp@gmail.com>
2020-11-01 14:13:28 -0500

    completed mvp

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const recognition  = new SpeechRecognition ()
 
 recognition .onstart = function () {
     console.log( "its working")
+   
 }
 
 recognition .onspeechend = function () {
     console.log( "end")
+  
 }
 
 recognition .onresult = function (event) {
     
-    let text = event.results[0][0].transcript
-    console.log( text)
-    content.textContent = text
+    let texts = event.results[0][0].transcript
+    console.log( texts)
+    content.textContent = texts;
+    readOut(texts);
+}
+
+function readOut(text) {
+    const speech = new SpeechSynthesisUtterance()
+
+    if (text.includes('who are you')){
+        speech.text =  "my name is Shadow, I am an artificial intelligence created by Stephen Plummer "
+    }
+
+    if (text.includes('who is Steven')){
+        speech.text =  "Stephen Plummer, is a one in a generational talent that you should add to  your team "
+    }
+
+    if (text.includes('why should i hire steven')){
+        speech.text =  "you should hire stephen becasue he has proven his ability to build efficient front-end interfaces with modern best practices."
+    }
+
+    if (text.includes('who is the most amazing person ever')){
+        speech.text =  "vanessa plummer is the most amazing person ever, not stephen"
+    }
+
+  
+    window.speechSynthesis.speak(speech)
+    console.log( "im reading the text")
 }
